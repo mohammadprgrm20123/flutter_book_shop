818e3d6ea509d9f6ffc8943a2c3a264850bc0a98
mohammadprgrm <kazeminejad95.itsu@yahoo.com>
2021-03-26 18:52:22 +0430

    refacor the search

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -10,63 +10,83 @@ class Search extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text('جستوجو',style:TextStyle(fontFamily: 'Dana', color: Colors.black, fontSize: 17.0),
-      ),
-      iconTheme: IconThemeData(color: Colors.black),
-      backgroundColor: Colors.white,
-      centerTitle: true,
-      ),
+      appBar: _appBar(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: CustomBtnNavigation().floatingActionButton(),
       bottomNavigationBar: CustomBtnNavigation().bottomNavigationBar(),
       body: Column(
         children: [
           //search view
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIconConstraints: BoxConstraints(),
-                  suffixIcon: IconButton(icon:Icon(Icons.delete_forever,color: Colors.red,),onPressed: (){},),
-                  labelText: S.of(context).search,
-                  hintText: S.of(context).search,
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25.0)))),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(itemBuilder: (_,int index){
-              return   Container(
-                height: 200.0,
-                child: ListView.builder(
-                  itemBuilder: (BuildContext _, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8.0),
-                        child: FadeInImage.assetNetwork(
-                          fadeInCurve: Curves.bounceIn,
-                          image:
-                          'https://imgcdn.taaghche.com/frontCover/90938.jpg?w=200',
-                          placeholder: 'assets/images/1.jpg',
-                        ),
-                      ),
-                    );
-                  },
-                  itemCount: 20,
-                  scrollDirection: Axis.horizontal,
-                ),
-              );
-
-
-              },
-               itemCount: 40,
-            ),
-          )
+          _serachView(context),
+          _listBooks()
         ],
       ),
     );
+  }
+
+  Expanded _listBooks() {
+    return Expanded(
+          child: ListView.builder(itemBuilder: (_,int index){
+            return   Container(
+              height: 200.0,
+              child: ListView.builder(
+                itemBuilder: (BuildContext _, int index) {
+                  return _itemBook();
+                },
+                itemCount: 20,
+                scrollDirection: Axis.horizontal,
+              ),
+            );
+
+
+            },
+             itemCount: 40,
+          ),
+        );
+  }
+
+  Padding _itemBook() {
+    return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: FadeInImage.assetNetwork(
+                      fadeInCurve: Curves.bounceIn,
+                      image:
+                      'https://imgcdn.taaghche.com/frontCover/90938.jpg?w=200',
+                      placeholder: 'assets/images/1.jpg',
+                    ),
+                  ),
+                );
+  }
+
+  Padding _serachView(BuildContext context) {
+    return Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIconConstraints: BoxConstraints(),
+                suffixIcon: IconButton(icon:Icon(Icons.delete_forever,color: Colors.red,),onPressed: (){},),
+                labelText: S.of(context).search,
+                hintText: S.of(context).search,
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+          ),
+        );
+  }
+
+  AppBar _appBar() {
+    return AppBar(title: _title(),
+    iconTheme: IconThemeData(color: Colors.black),
+    backgroundColor: Colors.white,
+    centerTitle: true,
+    );
+  }
+
+  Text _title() {
+    return Text('جستوجو',style:TextStyle(fontFamily: 'Dana', color: Colors.black, fontSize: 17.0),
+  );
   }
 
 }
